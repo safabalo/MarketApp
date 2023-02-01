2af4ffb0d81aaef5e7bda6a198556aca5a9c9a6f
safabalo <safaabalha58@gmail.com>
2023-02-01 08:26:29 +0000

    adding register screen

---
+++ src/screens/Register.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, SafeAreaView, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import Logo from '../assets/images/fruit.png';
+
+function Register() {
+    return (
+      <View style={styles.container}>
+        <Image source={Logo} style={{ width: 600, height:300}} />
+        {/* <Text>Welcome</Text> */}
+        <KeyboardAvoidingView style={styles.containerInputs} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <TextInput style={styles.input} />
+            <TextInput style={styles.secondInput} />
+        </KeyboardAvoidingView>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Register</Text>
+        </TouchableOpacity>
+      </View>
+      
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      flex: 1,
+      alignItems: 'center',
+    },
+    containerInputs: {
+      width: '100%',
+      alignItems: 'center',
+    },
+    input: {
+      width: "80%",
+      padding: 7,
+      borderWidth: 1,
+      borderColor: '#D6E4E5',
+      borderRadius: 5,
+    },
+    secondInput: {
+      width: "80%",
+      padding: 7,
+      borderWidth: 1,
+      borderColor: '#D6E4E5',
+      marginTop: 20,
+      borderRadius: 5,
+    },
+    button: {
+      backgroundColor: '#F57328',
+      width: '50%',
+      height: 40,
+      borderRadius: 5,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginTop: 20,
+    },
+    textButton: {
+      color: '#fff',
+      fontSize: 15,
+      fontWeight: 'bold',
+
+    },
+})
+export default Register;
